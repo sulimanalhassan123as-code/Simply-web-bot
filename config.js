0b981e419d06d09ced0fa7aaa9d6d48e58d76e1e
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2026-02-16 20:19:27 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-require("dotenv").config();
-
 module.exports = {
-    botName: "NeverHide SuperBot",
-    ownerNumber: process.env.OWNER_NUMBER,
-    prefix: ".",
-    version: "1.0.0"
+  botName: "NeverHide SuperBot"
 };
